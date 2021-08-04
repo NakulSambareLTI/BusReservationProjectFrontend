5435e5e45cdc70e692cade5e63e55607a79c0a07
NakulSambareLTI <Nakul.Sambare@lntinfotech.com>
2021-08-04 22:10:56 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyDashboardComponent
   ],
   imports: [
     BrowserModule,
